9bce6e6818d91c9095a53188edfbb4e1271dc1e5
Roberto75015 <roberto75015@gmail.com>
2026-09-15 01:47:25 +0200

    properly align --raw help text (no tabs)

--- help.go
+++ help.go
@@ -109,11 +109,11 @@ Options:
                                  Default: false
 
   --raw={true,false}             Switches text communication to raw mode, process
-				 read byte by byte and passes it to the counterpart
-				 without adding or removing \n to the messages.
-				 Please mote that, when using --passstderr, stderr
-				 is still handled on a line by line basis.
-				 Default: false
+                                 read byte by byte and passes it to the counterpart
+                                 without adding or removing \n to the messages.
+                                 Please mote that, when using --passstderr, stderr
+                                 is still handled on a line by line basis.
+                                 Default: false
 
   --passstderr                   Forward the process's STDERR to WebSocket
                                  clients, tagged (alongside STDOUT) as JSON:
